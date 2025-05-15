7d02765e8358cf5993e13d55288e1e94bd567647
Vivek9876543210 <151467373+Vivek9876543210@users.noreply.github.com>
2025-05-15 01:07:55 -0700

    Add files via upload

---
+++ QRScript.js
@@ -0,0 +1,23 @@
+let input = document.querySelector("#input");
+let button = document.querySelector("button");
+let QRimg = document.querySelector("#QRimg");
+let txt=document.querySelector("h3");
+function QRGenerator() {
+    
+    if (input.value=== "") {
+        alert("please Enter valid URL");
+        return;
+        input.focus();
+
+    }
+    else {
+        QRimg.setAttribute('src', 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + input.value);
+         input.focus();
+         input.value="";
+         console.log(txt.value);
+         
+         txt.innerHTML=input.value;
+    }
+}
+
+button.addEventListener("click",QRGenerator);
