6f9a39c7b375fb7c8a2dc05a0f2c3a8465a5a034
Vivek9876543210 <151467373+Vivek9876543210@users.noreply.github.com>
2025-05-18 13:59:35 +0530

    Update QRScript.js
    URL text see in QR Code below.

--- QRScript.js
+++ QRScript.js
@@ -1,7 +1,7 @@
 let input = document.querySelector("#input");
 let button = document.querySelector("button");
 let QRimg = document.querySelector("#QRimg");
-let txt=document.querySelector("h3");
+let txt=document.querySelector("p");
 function QRGenerator() {
     
     if (input.value=== "") {
@@ -12,11 +12,12 @@ function QRGenerator() {
     }
     else {
         QRimg.setAttribute('src', 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + input.value);
+         txt.innerHTML=input.value;
          input.focus();
          input.value="";
-         console.log(txt.value);
+         // console.log(txt.value);
          
-         txt.innerHTML=input.value;
+         
     }
 }
 
